fbef0ce2322f80fd2929d096cb473f20278afc21
SOMIDAX <23489369+somidax@users.noreply.github.com>
2018-03-28 14:57:12 +0100

    Update prep_Alert.py

--- scripts/prep_Alert.py
+++ scripts/prep_Alert.py
@@ -12,15 +12,15 @@ def get_link(token_info, link_key):
             return v
     return None
 
-def print_all_wrap(announcements):
-    for ann in announcements:
+def print_all_wrap(Alert):
+    for ann in Alert:
         print(ann, "\n\n")
 
 def twitter_style(token_info):
     """
-    Blah Token $BLAH by [@blah_com|blah.com] is now available on ForkDelta:
+    Blah Token $BLAH by [@SOMIDAX|somidax.net] is now available on coinEstate:
 
-    https://forkdelta.github.io/#!/trade/BLAH-ETH
+    https://coinEstate.somidax.net/#!/trade/SMDX-ETH
 
     #{symbol} #ERC20 #gems #Ethereum #ICO #Crypto #cryptotrading
     """
@@ -31,19 +31,19 @@ def twitter_style(token_info):
     else:
         issuer_attr = get_link(token_info, "Website")
 
-    symbol = token_info["symbol"] if not "__FORKDELTA_CUSTOM_SYMBOL" in token_info else token_info["__FORKDELTA_CUSTOM_SYMBOL"]
+    symbol = token_info["symbol"] if not "__COINESTATE_CUSTOM_SYMBOL" in token_info else token_info["__COINESTATE_CUSTOM_SYMBOL"]
 
     token_name = token_info["name"]
     if "token" not in token_name.lower():
         token_name += " Token"
 
-    return "{token_name} ${symbol} by {issuer_attr} is now available on @ForkDelta: https://forkdelta.github.io/#!/trade/{symbol}-ETH \n\n#{symbol} #ERC20 #gems #Ethereum #ICO #Crypto #cryptotrading".format(
+    return "{token_name} ${symbol} by {issuer_attr} is now available on @ForkDelta: https://coinEstate.somidax.net/#!/trade/{symbol}-ETH \n\n#{symbol} #ERC20 #gems #Ethereum #ICO #Crypto #cryptotrading".format(
         token_name=token_name, symbol=symbol, issuer_attr=issuer_attr,
     )
 
 def twitter_short_style(token_info):
     """
-    Blah Token $BLAH by [@blah_com|blah.com] https://forkdelta.github.io/#!/trade/BLAH-ETH
+    Blah Token $BLAH by [@SOMIDAX|somidax.net] https://coinEstate.somidax.net/#!/trade/SMDX-ETH
     """
 
     twitter = get_link(token_info, "Twitter")
@@ -52,21 +52,21 @@ def twitter_short_style(token_info):
     else:
         issuer_attr = get_link(token_info, "Website")
 
-    symbol = token_info["symbol"] if not "__FORKDELTA_CUSTOM_SYMBOL" in token_info else token_info["__FORKDELTA_CUSTOM_SYMBOL"]
+    symbol = token_info["symbol"] if not "__COINESTATE_CUSTOM_SYMBOL" in token_info else token_info["__COINESTATE_CUSTOM_SYMBOL"]
 
     token_name = token_info["name"]
     if "token" not in token_name.lower():
         token_name += " Token"
 
-    return "{token_name} ${symbol} by {issuer_attr} https://forkdelta.github.io/#!/trade/{symbol}-ETH #{symbol}".format(
+    return "{token_name} ${symbol} by {issuer_attr} https://coinEstate.somidax.net/#!/trade/{symbol}-ETH #{symbol}".format(
         token_name=token_name, symbol=symbol, issuer_attr=issuer_attr,
     )
 
-def twitter_short_wrap(announcements):
+def twitter_short_wrap(Alert):
     """
-    NEW LISTINGS
+    NEW LISTINGS NOTIFICATION!
 
-    <announcements || "\n\n" {0,3}>
+    <Alert || "\n\n" {0,3}>
 
     #ERC20 #gems #Ethereum #ICO #Crypto #cryptotrading
     """
@@ -74,17 +74,17 @@ def twitter_short_wrap(announcements):
 
 def reddit_style(token_info):
     """
-    **Blah Token $BLAH** by [blah.com](https://blah.com)
+    **Blah Token $SMDX** by [somidax.net](https://somidax.net)
 
     > <description>
 
-    https://forkdelta.github.io/#!/trade/BLAH-ETH
+    https://coinEstate.somidax.net/#!/trade/SMDX-ETH
     """
 
     website = get_link(token_info, "Website")
     website_name = urlparse(website).hostname
 
-    symbol = token_info["symbol"] if not "__FORKDELTA_CUSTOM_SYMBOL" in token_info else token_info["__FORKDELTA_CUSTOM_SYMBOL"]
+    symbol = token_info["symbol"] if not "__COINESTATE_CUSTOM_SYMBOL" in token_info else token_info["__COINESTATE_CUSTOM_SYMBOL"]
 
     token_name = token_info["name"]
     if "token" not in token_name.lower():
@@ -92,33 +92,9 @@ def reddit_style(token_info):
 
     description_quote = "> {}".format("\n> ".join(token_info.get("description", "").split("\n")))
 
-    return "**{token_name} ${symbol}** by [{website_name}]({website})  \n{description_quote}\n\nhttps://forkdelta.github.io/#!/trade/{symbol}-ETH".format(
+    return "**{token_name} ${symbol}** by [{website_name}]({website})  \n{description_quote}\n\nhttps://coinEstate.somidax.net/#!/trade/{symbol}-ETH".format(
         token_name=token_name, symbol=symbol, description_quote=description_quote, website=website, website_name=website_name
-    )
-
-def telegram_style(token_info):
-    """
-    **Blah Token $BLAH** by [blah.com](https://blah.com)
-
-    > <description>
-
-    https://forkdelta.github.io/#!/trade/BLAH-ETH
-    """
-
-    website = get_link(token_info, "Website")
-    website_name = urlparse(website).hostname
-
-    symbol = token_info["symbol"] if not "__FORKDELTA_CUSTOM_SYMBOL" in token_info else token_info["__FORKDELTA_CUSTOM_SYMBOL"]
-
-    token_name = token_info["name"]
-    if "token" not in token_name.lower():
-        token_name += " Token"
-
-    description_quote = "> {}".format("\n> ".join(token_info.get("description", "").split("\n")))
-
-    return "**{token_name} ${symbol}** by {website}  \n{description_quote}\n\nhttps://forkdelta.github.io/#!/trade/{symbol}-ETH".format(
-        token_name=token_name, symbol=symbol, description_quote=description_quote, website=website, website_name=website_name
-    )
+     )
 
 def discord_style(token_info):
     """
@@ -159,7 +135,6 @@ STYLE_TO_FUNC = {
     "github_response": { "each": github_response_style },
     "twitter": { "each": twitter_style },
     "twitter_batch": { "each": twitter_short_style, "wrap": twitter_short_wrap },
-    "telegram": { "each": telegram_style },
     "reddit": { "each": reddit_style },
 }
 
